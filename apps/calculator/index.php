2ab179e0068ccdbbbe0eaaa09878f2b48f5b3f8d
Charlie <charlieengler3@gmail.com>
2020-09-03 15:51:35 -0400

    Created the calculator and added calculator styling

---
+++ apps/calculator/index.php
@@ -0,0 +1,37 @@
+<html>
+	<head>
+		<link href="../../style/dark.css" rel="stylesheet">
+		
+		<title>Calculator</title>
+	</head>
+	
+	<body>
+		<h1>Calculator</h1>
+		
+		<div class="calculator">
+			<input type="text" readonly id="result">
+			
+			<button class="calculator_button" onclick="document.getElementById('result').value+='1';">1</button>
+			<button class="calculator_button" onclick="document.getElementById('result').value+='2';">2</button>
+			<button class="calculator_button" onclick="document.getElementById('result').value+='3';">3</button>
+			<button class="calculator_button" onclick="document.getElementById('result').value+='4';">4</button>
+			<br/>
+			<button class="calculator_button" onclick="document.getElementById('result').value+='5';">5</button>
+			<button class="calculator_button" onclick="document.getElementById('result').value+='6';">6</button>
+			<button class="calculator_button" onclick="document.getElementById('result').value+='7';">7</button>
+			<button class="calculator_button" onclick="document.getElementById('result').value+='8';">8</button>
+			<br/>
+			<button class="calculator_button" onclick="document.getElementById('result').value+='9';">9</button>
+			<button class="calculator_button" onclick="document.getElementById('result').value+='0';">0</button>
+			<button class="calculator_button" onclick="document.getElementById('result').value+='.';">.</button>
+			<button class="calculator_button" onclick="document.getElementById('result').value=eval(document.getElementById('result').value);">=</button>
+			<br/>
+			<button class="calculator_basic_operation_button" onclick="document.getElementById('result').value+='+';">+</button>
+			<button class="calculator_basic_operation_button" onclick="document.getElementById('result').value+='-';">-</button>
+			<button class="calculator_basic_operation_button" onclick="document.getElementById('result').value+='*';">×</button>
+			<button class="calculator_basic_operation_button" onclick="document.getElementById('result').value+='/';">÷</button>
+			<br/>
+			<button class="calculator_clear_button" onclick="document.getElementById('result').value='';">Clear</button>
+		</div>
+	</body>
+</html>
