7fb893f53d2da21934060d66f855d600d6d48afa
scheng7391 <sean7391@gmail.com>
2020-10-04 19:05:01 -0400

    Changed up GPA Calculator to allow for grade selection, class difficulty, and credits

--- apps/gpa_calculator/script.js
+++ apps/gpa_calculator/script.js
@@ -1,47 +1,40 @@
 //Global array definition
-var userLetterGrades = [];
+// var userLetterGrades = [];
 
-//Dropdown reference
-document.getElementById("letterGrades").value;
+var totalScore = 0, totalCredits = 0;
 
-//Function declaration
-function ProcessGrade()
-{
-    //Local array definition
-    let gradeThresholds = [95, 80, 70, 60];
-    let letterGrades = ["A", "B", "C", "D", "F"];
+function calcScore(level, rawGrade, credits){
+    if (level == "CP") return (rawGrade + 1.00) * credits;
+    else if (level == "AP/Honors") return (rawGrade + 2.00) * credits;
+    else return rawGrade * credits;
+}
 
-    //Local variable definition
-    let grade = document.getElementById("grade").value;
+function ProcessGrade(){
+    let gradeValues = [4.33, 4.00, 3.67, 3.33, 3.00, 2.67, 2.33, 2.00, 1.67, 1.33, 1.00, 0];
+    let letterGrades = ["A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "F"];
 
-    let numberGrade = parseFloat(grade);
+    let grade = document.getElementById("letterGrades").value;
+    let level = document.getElementById("difficulty").value;
+    let credit = document.getElementById("creditNumber").value;
+    totalCredits += parseInt(credit);
 
     let letterGradeValue;
-
-    //Simple loop
-    for(let i = 0; i < 4; i++)
-    {
-        if(numberGrade >= gradeThresholds[i])
-        {
+    for(let i = 0; i < 12; i++){
+        if(grade == letterGrades[i]){
             letterGradeValue = i;
             break;
         }
-        
-        if(i == 3 && numberGrade < gradeThresholds[i])
-        {
-            letterGradeValue = 4;
-        }
     }
 
-    //Reference to the header displaying the grade
-    document.getElementById("LetterGrade").innerHTML = letterGrades[letterGradeValue];
-
-    //Pushing a value into an array
-    userLetterGrades.push(letterGrades[letterGradeValue]);
+    totalScore += calcScore(level, gradeValues[letterGradeValue], credit);
+    let ans = (totalScore/totalCredits).toFixed(3);
+    document.getElementById("LetterGrade").innerHTML = ans;
 }
 
+/*
 function ShowFinal()
 {
     //Adding a new html element inside of a containing element
     document.getElementById("container").createElement("H3");
 }
+*/
