3d9594df1c920489c1609b93abce9daaed26787f
Charlie <charlieengler3@gmail.com>
2020-10-04 20:18:38 -0400

    Set up basic html file for pomodoro timer

---
+++ apps/pomodoro_timer/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Pomodoro Timer</title>
+
+        <link rel="stylesheet" href="../../style/dark.css"/>
+        <script src="script.js" defer></script>
+    </head>
+
+    <body>
+
+    </body>
+</html>
