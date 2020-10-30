2a71e033c604cd4637f143904422b8f9aac3af64
Charlie <charlieengler3@gmail.com>
2020-10-30 17:46:44 -0400

    Added the ability to edit comments on the forums

--- apps/forums/comment.php
+++ apps/forums/comment.php
@@ -1,57 +1,116 @@
 ﻿<?php
-  session_start();
+	session_start();
 
-  $title = $_POST['title'];
-  $comment = $_POST['comment'];
+	if(isset($_POST['title']))
+	{
+		$title = $_POST['title'];
+	}
 
-  $user = $_SESSION['username'];
+	if(isset($_GET['title']))
+	{
+		$title = $_GET['title'];
+	}
+?>
+<html>
+	<head>
+		<title><?php echo $title." Edit Comment"; ?></title>
+
+		<link rel="stylesheet" type="text/css" href="../../style/dark.css"/>
+
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+
+		<div class='user_bar'>
+			<a href="show_post.php?title=<?php echo $title; ?>">Back</a>
+		</div>
+
+		<script type="text/javascript">
+			function formAutoSubmit(){
+				var frm = document.getElementById("autoSubmit");
+				frm.submit();
+			}
+			window.onload = formAutoSubmit();
+		</script>
+	</head>
+<?php
+  	$username = $_SESSION['username'];
 
 	include("../../../password.php");
 
-  $servername = "localhost";
-  $server_user = "root";
+	$servername = "localhost";
+	$server_user = "root";
 
 	$conn = new mysqli($servername, $server_user, $serverpassword, "forums");
-	
-  $comment = $comment."⎖";
-  $commentUser = $user."⎖";
+  
+	if(isset($_POST['comment']))
+	{
+		$title = $_POST['title'];
+		$comment = $_POST['comment'];
 
-  $result = $conn->query("SELECT * FROM posts WHERE title='$title'");
+		if(isset($_POST['edited_comment']))
+		{
+			$edited_comment = $_POST['edited_comment'];
 
-  if($result->num_rows != 0)
-  {
-    $row = $result->fetch_assoc();
+			$result = $conn->query("SELECT * FROM posts WHERE title='$title'");
 
-    $allComments = $row['comments'];
-    $allUsers = $row['comment_users'];
-  }
-  else
-  {
-    $allComments = "";
-    $allUsers = "";
-  }
+			while($row = $result->fetch_assoc())
+			{
+				$comments = $row['comments'];
 
-  $allComments = $allComments.$comment;
-  $allUsers = $allUsers.$commentUser;
+				$title = $_POST['title'];
 
-  echo $allComments."<br>";
-  echo $allUsers."<br>";
+				$comments = str_replace($edited_comment, $_POST['comment'], $comments);
 
-  $conn->query("UPDATE posts SET comments='$allComments' WHERE user='$user'");
-  $conn->query("UPDATE posts SET comment_users='$allUsers' WHERE user='$user'");
+				$conn->query("UPDATE posts SET comments='$comments' WHERE title='$title'");
 
-  echo "<form id='autoSubmit' action='show_post.php' method='POST'><input type='hidden' name='title' value='".$title."'/></form>";
+				echo "<script>location.href='show_post.php?title=".$title."'</script>";
+			}
+		}
+		else
+		{
+			$comment = $comment."⎖";
+			$commentUser = $username."⎖";
+
+			$result = $conn->query("SELECT * FROM posts WHERE title='$title'");
+
+			if($result->num_rows != 0)
+			{
+				$row = $result->fetch_assoc();
+
+				$allComments = $row['comments'];
+				$allUsers = $row['comment_users'];
+			}
+			else
+			{
+				$allComments = "";
+				$allUsers = "";
+			}
+
+			$allComments = $allComments.$comment;
+			$allUsers = $allUsers.$commentUser;
+
+			echo $allComments."<br>";
+			echo $allUsers."<br>";
+
+			$conn->query("UPDATE posts SET comments='$allComments' WHERE user='$username'");
+			$conn->query("UPDATE posts SET comment_users='$allUsers' WHERE user='$username'");
+		}
+
+		echo "<form id='autoSubmit' action='show_post.php' method='POST'><input type='hidden' name='title' value='".$title."'/></form>";
+	}
+
+	if(isset($_GET['edit_comment']))
+	{
+		$title = $_GET['title'];
+		$comment = $_GET['edit_comment'];
+
+		?>
+			<form action="#" class='comments' method="POST" autocomplete=off>
+				<input type="text" name="comment" class='comment_field' value="<?php echo $comment; ?>"/>
+				<input type="hidden" name="edited_comment" value="<?php echo $comment; ?>"/>
+				<input type="hidden" name="title" value="<?php echo $title; ?>"/>
+				<input type="submit" class='submit_comment' value="Submit"/>
+			</form>
+		<?php
+	}
 ?>
-
-<script type="text/javascript">
-
-  function formAutoSubmit()
-  {
-    var frm = document.getElementById("autoSubmit");
-
-    frm.submit();
-  }
-
-  window.onload = formAutoSubmit();
-
-</script>
+</html>
--- apps/forums/show_post.php
+++ apps/forums/show_post.php
@@ -1,7 +1,15 @@
 <?php
-  session_start();
+  	session_start();
 
-  $title = $_POST['title'];
+	if(isset($_POST['title']))
+	{
+		$title = $_POST['title'];
+	}
+	  
+	if(isset($_GET['title']))
+	{
+		$title = $_GET['title'];
+	}
 
 	include("../../../password.php");
 
@@ -10,135 +18,148 @@
   
 	$conn = new mysqli($servername, $server_user, $serverpassword, "forums");
 	  
-  $result = $conn->query("SELECT * FROM posts WHERE title = '$title'");
+	$result = $conn->query("SELECT * FROM posts WHERE title = '$title'");
 
-  while($row = $result->fetch_assoc())
-  {
-    $user = $row['user'];
-    $body = $row['body'];
-    $image_links = $row['image_links'];
-    $comments = $row['comments'];
-    $comment_users = $row['comment_users'];
-  }
+	while($row = $result->fetch_assoc())
+	{
+		$user = $row['user'];
+		$body = $row['body'];
+		$image_links = $row['image_links'];
+		$comments = $row['comments'];
+		$comment_users = $row['comment_users'];
+	}
 ?>
 
 <html>
   <head>
-    <title><?php echo $title; ?></title>
-    
-    <link rel="stylesheet" type="text/css" href="../../style/dark.css"/>
+	<title><?php echo $title; ?></title>
+	
+	<link rel="stylesheet" type="text/css" href="../../style/dark.css"/>
 
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    
-    <div class='user_bar'>
-      <a href='index.php'>Home</a>
-    </div>
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+	
+	<div class='user_bar'>
+		<a href='../../index.php'>Home</a>
+	</div>
 
-    <?php
-      if(isset($_SESSION['username']))
-      {
-        if($user == $_SESSION['username'])
-        {
-          echo "<div class='user_bar'><a onclick='submitEdit()'>Edit</a></div>";
+	<div class='user_bar'>
+		<a href='index.php'>Forums</a>
+	</div>
+	
+	<?php
+	  if(isset($_SESSION['username']))
+	  {
+		if($user == $_SESSION['username'])
+		{
+		  echo "<div class='user_bar'><a onclick='submitEdit()'>Edit</a></div>";
 
-          ?>
-            <form id="editSubmit" method="POST" action="edit.php"><input type="hidden" name="title" value="<?php echo $title; ?>"/></form>
-          <?php
-        }
-      }
-    ?>
+		  ?>
+			<form id="editSubmit" method="POST" action="edit.php"><input type="hidden" name="title" value="<?php echo $title; ?>"/></form>
+		  <?php
+		}
+	  }
+	?>
   </head>
   
   <body>
-    <h1><u>
-      <?php echo $title; ?>
-    </u></h1>
-    <div class='author'>
-      By: <?php echo $user ?>
-    </div>
-    
-    <div class='post'>
-      <?php 
-        echo $body; 
+	<h1><u>
+	  <?php echo $title; ?>
+	</u></h1>
+	<div class='author'>
+	  By: <?php echo $user ?>
+	</div>
+	
+	<div class='post'>
+	  <?php 
+		echo $body; 
 
-        if($image_links != "")
-        {
-      ?>
-      
-      <div id='images' class='images'>
-        Click on images to expand them.
-        <br>
-        <br>
-          <?php
-            $image_links_arr = explode("⎖", $image_links);
+		if($image_links != "")
+		{
+	  ?>
+	  
+	  <div id='images' class='images'>
+		Click on images to expand them.
+		<br>
+		<br>
+		  <?php
+			$image_links_arr = explode("⎖", $image_links);
 
-            for($i = 0; $i < sizeof($image_links_arr); $i++)
-            {
-              echo "<img onclick='ExpandImage(\"".$image_links_arr[$i]."\");' src='".$image_links_arr[$i]."'>";
-            }
-          ?>
-      </div>
+			for($i = 0; $i < sizeof($image_links_arr); $i++)
+			{
+			  echo "<img onclick='ExpandImage(\"".$image_links_arr[$i]."\");' src='".$image_links_arr[$i]."'>";
+			}
+		  ?>
+	  </div>
 
-      <?php
-        }
-      ?>
-    </div>
+	  <?php
+		}
+	  ?>
+	</div>
 
-    
-    
-    <div class="comments">
-      <h3>
-        Comments:
-      </h3>
+	
+	
+	<div class="comments">
+	  <h3>
+		Comments:
+	  </h3>
 
-      <?php
-        if(isset($_SESSION['username']))
-        {
-      ?>
-          <form action="comment.php" method="POST" autocomplete=off>
-            <input type="text" name="comment" class='comment_field' placeholder="Add a comment"/>
-            <input type="hidden" name="title" value="<?php echo $title; ?>"/>
-            <input type="submit" class='submit_comment' value="Submit"/>
-          </form>
-      <?php
-        }
-      ?>
-      
-      <?php
-        $commentArray = explode("⎖", $comments);
-        $userArray = explode("⎖", $comment_users);
-        
-        for($i = 0; $i < sizeof($commentArray); $i++)
-        {
-          if($commentArray[$i] != "")
-          {
-            echo "<div class='comment'>".$commentArray[$i]."<div class='comment_user'>By: ".$userArray[$i]."</div></div>";
-          }
-        }
-      ?>
-    </div>
+	  <?php
+		if(isset($_SESSION['username']))
+		{
+	  ?>
+			<form action="comment.php" method="POST" autocomplete=off>
+				<input type="text" name="comment" class='comment_field' placeholder="Add a comment"/>
+				<input type="hidden" name="title" value="<?php echo $title; ?>"/>
+				<input type="submit" class='submit_comment' value="Submit"/>
+			</form>
+	  <?php
+		}
+	  ?>
+	  
+		<?php
+			$commentArray = explode("⎖", $comments);
+			$userArray = explode("⎖", $comment_users);
+			
+			for($i = 0; $i < sizeof($commentArray); $i++)
+			{
+				if($commentArray[$i] != "")
+				{
+				echo "<div class='comment'>".$commentArray[$i]."<div class='comment_user'>By: ".$userArray[$i]." ";
+				if(isset($_SESSION['username']))
+				{
+					if($userArray[$i] == $_SESSION['username'])
+					{
+						echo "<a href='comment.php?title=".$title."&edit_comment=".$commentArray[$i]."'>Edit</a>";
+					}
+				}
+				
+				echo "</div></div>";
+				}
+			}
+		?>
+	</div>
 
-    <script>
-      function submitEdit()
-      {
-        var frm = document.getElementById("editSubmit");
+	<script>
+	  function submitEdit()
+	  {
+		var frm = document.getElementById("editSubmit");
 
-        frm.submit();
-      }
-      
-      function ExpandImage(link)
-      {
-        if(!document.getElementById("expanded_image"))
-        {
-          var expandedImage = document.createElement("IMG");
-          expandedImage.className = "expanded_image";
-          expandedImage.onclick = function() {document.getElementById("expanded_image").remove();};
-          expandedImage.id = "expanded_image";
-          expandedImage.src = link.toString();
+		frm.submit();
+	  }
+	  
+	  function ExpandImage(link)
+	  {
+		if(!document.getElementById("expanded_image"))
+		{
+		  var expandedImage = document.createElement("IMG");
+		  expandedImage.className = "expanded_image";
+		  expandedImage.onclick = function() {document.getElementById("expanded_image").remove();};
+		  expandedImage.id = "expanded_image";
+		  expandedImage.src = link.toString();
 
-          document.body.appendChild(expandedImage);
-        }
-      }
-    </script>
+		  document.body.appendChild(expandedImage);
+		}
+	  }
+	</script>
   </body>
 </html>
